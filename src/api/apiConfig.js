8c94cc01ade55ac23846dac64a67e648b5e54246
QuickScouting <94945213+QuickScouting@users.noreply.github.com>
2022-03-23 23:13:12 +0700

    thu buoi

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: 'get from themoviedb.org',
+    apiKey: '15170828fce2ef8682554b4cf2eaba4e',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
